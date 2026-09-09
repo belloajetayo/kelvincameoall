ffd0f67aa9816d53064e05881e04c853a15e0acf
belloajetayo <belloajetayo@users.noreply.github.com>
2026-09-09 12:35:39 +0100

    auto: live update from Kelvin Cameo studio

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
   initExperiencePicker();
   initConciergeEstimator();
   initFaqAccordion();
+  initSiteKitAnalytics();
 });
 
 /* --------------------------------------------------------------------------
